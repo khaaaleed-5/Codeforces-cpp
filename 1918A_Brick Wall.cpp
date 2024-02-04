22b9047a61c5c1d427fe63056a9d65ff05863d9a
Khaled Ashraf <105022395+khaaaleed-5@users.noreply.github.com>
2024-02-04 18:34:05 +0200

    Update 1918A_Brick Wall.cpp

--- 1918A_Brick Wall.cpp
+++ 1918A_Brick Wall.cpp
@@ -42,69 +42,6 @@ struct ValueComparator {
     }
 };
 
-bool prime(ll n) {
-    if (n == 1 || n == 0)
-        return 0;
-    for (ll i = 2; i * i <= n; i++)
-        if (n % i == 0)
-            return 0;
-    return 1;
-}
-
-bool isSubSequence(string str1, string str2, int m, int n) {
-    if (m == 0)
-        return true;
-    if (n == 0)
-        return false;
-
-    if (str1[m - 1] == str2[n - 1])
-        return isSubSequence(str1, str2, m - 1, n - 1);
-    return isSubSequence(str1, str2, m, n - 1);
-}
-
-vecl fib() {
-    vecl dp(50 + 1);
-    dp[0] = 0, dp[1] = 1;
-    for (ll i = 2; i <= 50; i++) {
-        dp[i] = dp[i - 1] + dp[i - 2];
-    }
-    return dp;
-}
-
-
-int SubArray(int a[], int size) {
-    int ma = INT_MIN, end_point = 0;
-    for (int i = 0; i < size; i++) {
-        end_point = end_point + a[i];
-        if (ma < end_point)
-            ma = end_point;
-        if (end_point < 0)
-            end_point = 0;
-    }
-    return ma;
-}
-
-void fract(int num, int d) {
-    for (int i = d; i >= 2; i--) {
-        if (num % i == 0 && d % i == 0) {
-            num = num / i;
-            d = d / i;
-        }
-    }
-    if (d == 1)
-        cout << num;
-    else
-        cout << num << "/" << d;
-}
-
-bool even(ll n) {
-    return n & 1 ^ 1;
-}
-
-const int MAX = 10e5;
-int frqA[MAX];
-
-
 void solve() {
     int n, m;
     cin >> n >> m;
@@ -113,8 +50,6 @@ void solve() {
 
 int main() {
     Khaaaleed_5
-//    freopen("input.txt", "r", stdin);
-//    freopen("output.txt", "w", stdout);
     why {
         solve();
         nl;
