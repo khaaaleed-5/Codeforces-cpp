098cdadf7f07386acf6a0f265ff3e248edca4990
Khaled Ashraf <105022395+khaaaleed-5@users.noreply.github.com>
2024-02-19 00:29:41 +0200

    Create 1929B_Sasha and the Drawing.cpp

---
+++ 1929B_Sasha and the Drawing.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+#include <algorithm>
+
+#define Khaaaleed_5 (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+#define re return
+#define dsort      sort(arr,arr+n,greater<>())
+#define asort      sort(arr,arr+n)
+#define all(v)     v.begin(),v.end()
+#define veci vector<int>
+#define vecl vector<ll>
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define dd double
+#define nl cout<<"\n"
+#define no cout<<"NO"
+#define yes cout<<"YES"
+#define why int t;cin>>t; while(t--)
+
+using namespace std;
+
+template<typename S>
+ostream &operator<<(ostream &os, const vector<S> &vector) {
+    for (auto element: vector) {
+        os << element << " ";
+    }
+    re os;
+}
+
+void solve() {
+    ll n, k;
+    cin >> n >> k;
+    if (k <= 4 * n - 4)
+        return void(cout << (k + 1) / 2);
+    cout << k / 2 + 1;
+}
+
+int main() {
+    Khaaaleed_5
+    why {
+        solve();
+        nl;
+    }
+
+    return 0;
+}
