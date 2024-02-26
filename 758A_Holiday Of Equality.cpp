d74452da58eacc3abc486672234abb333c4da0a3
Khaled Ashraf <105022395+khaaaleed-5@users.noreply.github.com>
2024-02-27 00:24:13 +0200

    Create 758A_Holiday Of Equality.cpp

---
+++ 758A_Holiday Of Equality.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+#include <algorithm>
+
+#define Khaaaleed_5 (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+#define re return
+#define dsort      sort(arr,arr+n,greater<>())
+#define asort      sort(arr,arr+n)
+#define all(v)     v.begin(),v.end()
+#define veci vector<int>
+#define vecl vector<ll>
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define dd double
+#define nl cout<<"\n"
+#define no cout<<"NO"
+#define yes cout<<"YES"
+#define why int t;cin>>t; while(t--)
+
+using namespace std;
+
+template<typename S>
+ostream &operator<<(ostream &os, const vector<S> &vector) {
+    for (auto element: vector) {
+        os << element << " ";
+    }
+    re os;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    veci v(n);
+    for (auto &i: v)
+        cin >> i;
+    int max_elem = *max_element(v.begin(), v.end());
+    ll cnt = 0;
+    for(auto i:v){
+        cnt += max_elem - i;
+    }
+    cout << cnt;
+}
+
+int main() {
+    Khaaaleed_5
+//    why {
+        solve();
+//        nl;
+//    }
+
+    return 0;
+}
