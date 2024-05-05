abbfdaf5ac11aa98f810c4a51171a05d2ef87a3a
Khaled Ashraf <105022395+khaaaleed-5@users.noreply.github.com>
2024-05-05 23:45:43 +0300

    Create 1968B_Prefiquence.cpp

---
+++ 1968B_Prefiquence.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+#include <algorithm>
+
+#define Khaaaleed_5 (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+#define re return
+#define dsort      sort(arr,arr+n,greater<>())
+#define asort      sort(arr,arr+n)
+#define all(v)     v.begin(),v.end()
+#define veci vector<int>
+#define vecl vector<ll>
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define dd double
+#define nl cout<<"\n"
+#define no cout<<"NO"
+#define yes cout<<"YES"
+#define why int t;cin>>t; while(t--)
+
+using namespace std;
+
+template<typename S>
+ostream &operator<<(ostream &os, const vector<S> &vector) {
+    for (auto element: vector) {
+        os << element << " ";
+    }
+    re os;
+}
+
+bool compareStrings(string &s, string &ss) {
+    if (s.length() != ss.length()) {
+        return s.length() < ss.length();
+    } else {
+        return s < ss;
+    }
+}
+
+template<typename K, typename V>
+struct ValueComparator {
+    bool operator()(const std::pair<K, V> &lhs, const std::pair<K, V> &rhs) const {
+        return lhs.second < rhs.second;
+    }
+};
+
+void solve() {
+    int n, m, l1 = 0, l2 = 0;
+    cin >> n >> m;
+    string a, b;
+    cin >> a >> b;
+    int s = 0;
+    while (l1 < n and l2 < m) {
+        if (a[l1] == b[l2]) {
+            s += 1;
+            l1 += 1;
+        }
+        l2 += 1;
+    }
+    cout << s;
+}
+
+int main() {
+    Khaaaleed_5
+    why {
+        solve();
+        nl;
+    }
+
+    return 0;
+}
