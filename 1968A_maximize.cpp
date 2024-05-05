8b01511c54b8893e3f6c7d8876fe39e759b5ad92
Khaled Ashraf <105022395+khaaaleed-5@users.noreply.github.com>
2024-05-05 23:44:31 +0300

    Create 1968A_maximize.cpp

---
+++ 1968A_maximize.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+#include <algorithm>
+
+#define Khaaaleed_5 (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+#define re return
+#define dsort      sort(arr,arr+n,greater<>())
+#define asort      sort(arr,arr+n)
+#define all(v)     v.begin(),v.end()
+#define veci vector<int>
+#define vecl vector<ll>
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define dd double
+#define nl cout<<"\n"
+#define no cout<<"NO"
+#define yes cout<<"YES"
+#define why int t;cin>>t; while(t--)
+
+using namespace std;
+
+template<typename S>
+ostream &operator<<(ostream &os, const vector<S> &vector) {
+    for (auto element: vector) {
+        os << element << " ";
+    }
+    re os;
+}
+
+bool compareStrings(string &s, string &ss) {
+    if (s.length() != ss.length()) {
+        return s.length() < ss.length();
+    } else {
+        return s < ss;
+    }
+}
+
+template<typename K, typename V>
+struct ValueComparator {
+    bool operator()(const std::pair<K, V> &lhs, const std::pair<K, V> &rhs) const {
+        return lhs.second < rhs.second;
+    }
+};
+
+
+void solve() {
+    int x;
+    cin >> x;
+    cout << x - 1;
+}
+
+int main() {
+    Khaaaleed_5
+    why {
+        solve();
+        nl;
+    }
+
+    return 0;
+}
